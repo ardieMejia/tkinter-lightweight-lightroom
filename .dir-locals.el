21e53be65f9ce228f3163afdc7c0f345e0aa4057
Wan Ardie <wan_ahmad_ardie@yahoo.com>
2025-07-23 10:59:15 +0800

    dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,9 @@
-((python-mode . ((ardie/project-shell-command . "python main.py --myname ardie")
+(
+ (python-mode . ((ardie/project-shell-command . "python main.py --myname ardie")
          ;; (fill-column . 80)
          ;; (mode . auto-fill)
+	 ))
+ (python-ts-mode . ((ardie/project-shell-command . "python main.py --myname ardie")
 	 ))
  ;; (c-mode . ((c-file-style . "BSD")
  ;;            (subdirs . nil)))
